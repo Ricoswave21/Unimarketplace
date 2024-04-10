1e27c5304ab9c7b2c8bc6ae4a7cfc3b4b22ea12d
adamareberg <164181372+adamareberg@users.noreply.github.com>
2024-04-10 16:19:23 +0200

    Add files via upload

---
+++ BA/items/views.py
@@ -0,0 +1,11 @@
+from .models import Items
+from django.shortcuts import render, get_object_or_404
+# Create your views here.
+def detail(request, pk):
+    item = get_object_or_404(Items, pk=pk)
+    related_items = Items.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })

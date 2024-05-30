1bdadfe21aa045f5df6fdae17ea509c4685dad45
helinzamar <helinzamar330@gmail.com>
2024-05-30 20:46:09 +0200

    Helin,delete oh edit
    Jag har gjort att användare kan ta bort produkten som hen har laddat och även kan redigera annons/produkten som hen har lagt till. Detta görs bara när man är inloggad.

--- Unimarketplace-Backend 2/BA/items/forms.py
+++ Unimarketplace-Backend 2/BA/items/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Items
+
+
 INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+
 
 class NewitemForm(forms.ModelForm):
     class Meta:
@@ -10,6 +14,25 @@ class NewitemForm(forms.ModelForm):
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
             }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            })
+        }
+
+class EdititemForm(forms.ModelForm):
+    class Meta:
+        model = Items
+        fields=("name","description","price","image", 'is_sold')
+        widgets = {
             'name': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
             }),
